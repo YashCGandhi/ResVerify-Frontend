a59414346ec019d8896972685cba85ce4351163a
Yash Chinmay Gandhi <Yashgandhi99@gmail.com>
2023-08-14 13:23:53 -0700

    minor bug

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Nft from "./components/nft/Nft";
 import Login from "./components/login/Login";
-import Test from "./components/test/Test";
+// import Test from "./components/test/Test";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/test" element={<Test />} />
+        {/* <Route path="/test" element={<Test />} /> */}
         <Route path="/home" element={<Home />} />
         <Route path="/nft" element={<Nft />} />
       </Routes>
