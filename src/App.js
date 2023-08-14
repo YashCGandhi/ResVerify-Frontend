0d00ada5e6fb37f3820cb69872f1a5b1f2c5dc8a
Yash Chinmay Gandhi <Yashgandhi99@gmail.com>
2023-08-14 13:22:04 -0700

    adding login and auth using descope

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Nft from "./components/nft/Nft";
+import Login from "./components/login/Login";
+import Test from "./components/test/Test";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/nft" element={<Nft />} />
       </Routes>
     </Router>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,39 @@
-import React, { createRef, useState } from "react";
+import React, { createRef, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
 import CheckIcon from "@mui/icons-material/Check";
+import {
+  useDescope,
+  useUser,
+  getSessionToken,
+  useSession,
+} from "@descope/react-sdk";
+import { Descope } from "@descope/react-sdk";
 import "./Home.css";
 
 const Home = () => {
+  const { isAuthenticated, isSessionLoading } = useSession();
+  const { user, isUserLoading } = useUser();
+  const { logout } = useDescope();
+
   const navigate = useNavigate();
+
+  const exampleFetchCall = async () => {
+    const sessionToken = getSessionToken();
+
+    // example fetch call with authentication header
+    fetch("your_application_server_url", {
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + sessionToken,
+      },
+    });
+  };
+
+  const handleLogout = useCallback(() => {
+    logout();
+  }, [logout]);
+
   const [resume, setResume] = useState(null);
   const fileInput = createRef();
 
@@ -45,41 +73,59 @@ const Home = () => {
   };
 
   return (
-    <div className="home-page">
-      <Navbar />
-      {/* <form onSubmit={onSubmit}>
-        <input type='file' name="resume" ref={fileInput}/>
-        <input type="submit" value="Submit"/>
-    </form> */}
+    <>
+      {!isAuthenticated && (
+        <Descope
+          flowId="sign-up-or-in"
+          onSuccess={(e) => console.log(e.detail.user)}
+          onError={(e) => console.log("Could not log in!")}
+        />
+      )}
 
-      <div className="file-upload">
-        <form onSubmit={onSubmit}>
-          <div className="file-upload-wrap">
-            <input
-              className="file-upload-input"
-              type="file"
-              name="resume"
-              ref={fileInput}
-            />
-            <div className="drag-text">
-              {resume !== null ? (
-                <h3> {resume}</h3>
-              ) : (
-                <h3> Select Your Resume</h3>
-              )}
+      {(isSessionLoading || isUserLoading) && <p>Loading...</p>}
+
+      {!isUserLoading && isAuthenticated && (
+        <>
+          <div className="home-page">
+            <Navbar />
+            <div className="file-upload">
+              <form onSubmit={onSubmit}>
+                <div className="file-upload-wrap">
+                  <input
+                    className="file-upload-input"
+                    type="file"
+                    name="resume"
+                    ref={fileInput}
+                  />
+                  <div className="drag-text">
+                    {resume !== null ? (
+                      <h3> {resume}</h3>
+                    ) : (
+                      <h3> Select Your Resume</h3>
+                    )}
+                  </div>
+                </div>
+                <button
+                  className="file-upload-btn"
+                  type="submit"
+                  value="Upload"
+                >
+                  <CheckIcon /> <p>Upload</p>
+                </button>
+                {/* <input className="file-upload-btn" type="submit" value="Upload"></input> */}
+              </form>
             </div>
-          </div>
-          <button className="file-upload-btn" type="submit" value="Upload">
-            <CheckIcon /> <p>Upload</p>
-          </button>
-          {/* <input className="file-upload-btn" type="submit" value="Upload"></input> */}
-        </form>
-      </div>
 
-      <button className="next-btn" onClick={navigateToNFT}>
-        Next
-      </button>
-    </div>
+            <button className="next-btn" onClick={navigateToNFT}>
+              Next
+            </button>
+          </div>
+          <p>Hello {user.name}</p>
+          <div>My Private Component</div>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      )}
+    </>
   );
 };
 
