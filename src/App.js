cd542d53694347ef694d4059cd38e5329284826a
Yash Chinmay Gandhi <Yashgandhi99@gmail.com>
2023-08-14 15:15:16 -0700

    login page at /

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         {/* <Route path="/test" element={<Test />} /> */}
         <Route path="/home" element={<Home />} />
         <Route path="/nft" element={<Nft />} />
