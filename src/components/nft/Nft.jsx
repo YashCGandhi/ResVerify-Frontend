770ce80b2c6a0555e90b6369f5fe85510702d664
Yash Chinmay Gandhi <Yashgandhi99@gmail.com>
2023-08-14 07:31:03 -0700

    function to convert raw json to key, value pairs of org and token

--- src/components/nft/Nft.jsx
+++ src/components/nft/Nft.jsx
@@ -1,9 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import ClearIcon from "@mui/icons-material/Clear";
 import "./Nft.css";
 
 const Nft = () => {
+  const [rawData, setRawData] = useState([
+    {
+      accreditation: {
+        education: "PhD in Computer Science and Engineering",
+        input_str: "PhD in Computer Science and Engineering",
+        match_str: "",
+      },
+      dates: {
+        completion_date: "2021-09-01",
+        is_current: false,
+        raw_text: "Sep 2021- Aug 2027 (Exp)",
+      },
+      grade: { metric: "GPA", raw: "GPA: 3.52/4.00", value: "3.52/4.00" },
+      id: 30146059,
+      location: {
+        city: "Santa Cruz",
+        country: "United States",
+        country_code: "US",
+        formatted: "Santa Cruz, CA, USA",
+        latitude: 36.9741171,
+        longitude: -122.0307963,
+        raw_input: "Santa Cruz",
+        state: "California",
+      },
+      organization: "University of California",
+    },
+    {
+      accreditation: {
+        education: "Bachelor of Engineering",
+        education_level: "bachelors",
+        input_str: "Bachelor of Engineering in Computer Engineering",
+        match_str: "",
+      },
+      dates: {
+        completion_date: "2021-06-01",
+        is_current: false,
+        raw_text: "Aug 2017 - Jun 2021",
+        start_date: "2017-08-01",
+      },
+      grade: { metric: "CGPA", raw: "CGPA: 8.26/10.00", value: "8.26/10.00" },
+      id: 30146060,
+      organization:
+        "K.J Somaiya Institute of Engineering & I.T, University of Mumbai",
+    },
+  ]);
+
   const [formData, setFormData] = useState([
     { key: "lasjdflka;sjfl;asjdflak", value: "Initial Value" },
     { key: "Author", value: "John Doe" },
@@ -11,6 +57,19 @@ const Nft = () => {
     { key: "Date", value: "2022-08-12" },
   ]);
 
+  const rawToFormData = (rawD) => {
+    const formArray = [];
+    rawD.forEach((obj) => {
+      const org = obj["organization"];
+      const randomToken = Math.random();
+      formArray.push({ key: org, value: randomToken });
+    });
+    console.log(formArray);
+    setFormData(formArray);
+  };
+  useEffect(() => {
+    rawToFormData(rawData);
+  }, [rawData]);
   const handleChange = (e, index) => {
     const { name, value } = e.target;
     const updatedFormData = [...formData];
